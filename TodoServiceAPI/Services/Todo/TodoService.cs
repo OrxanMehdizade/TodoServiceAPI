429d301bd799546bf941d360c8b2c4696ad8be89
Mehdi_ha01 <mehdizadeorxan2000@gmail.com>
2024-02-04 12:49:12 +0400

    update try catch

--- TodoServiceAPI/Services/Todo/TodoService.cs
+++ TodoServiceAPI/Services/Todo/TodoService.cs
@@ -17,93 +17,136 @@ namespace TodoServiceAPI.Services.Todo
 
         public async Task<TodoItemDto> ChangeTodoItemStatus(int id, bool isCompleted)
         {
-            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
-            if (todoItem == null)
+            try
             {
-                return null;
+                var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+                if (todoItem == null)
+                {
+                    return null;
+                }
+
+                todoItem.IsCompleted = isCompleted;
+                await _context.SaveChangesAsync();
+
+
+                return new TodoItemDto(
+                        id: todoItem.Id,
+                        text: todoItem.Text,
+                        isCompleted: todoItem.IsCompleted,
+                        createdTime: todoItem.CreatedTime);
+
             }
-
-            todoItem.IsCompleted = isCompleted;
-            await _context.SaveChangesAsync();
-
-
-            return new TodoItemDto(
-                    id: todoItem.Id,
-                    text: todoItem.Text,
-                    isCompleted: todoItem.IsCompleted,
-                    createdTime: todoItem.CreatedTime);
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error in ChangeTodoItemStatus:{ex.Message}");
+                throw;
+            }
 
         }
 
         public async Task<TodoItemDto> CreateTodo(CreateTodoItemRequest request)
         {
-            var todoItem = new TodoItem
+            try
             {
-                Text = request.Text,
-                IsCompleted = false,
-                CreatedTime = DateTime.Now
-            };
+                var todoItem = new TodoItem
+                {
+                    Text = request.Text,
+                    IsCompleted = false,
+                    CreatedTime = DateTime.Now
+                };
 
-            await _context.TodoItems.AddAsync(todoItem);
-            await _context.SaveChangesAsync();
+                await _context.TodoItems.AddAsync(todoItem);
+                await _context.SaveChangesAsync();
 
-            return new TodoItemDto(
-                    id: todoItem.Id,
-                    text: todoItem.Text,
-                    isCompleted: todoItem.IsCompleted,
-                    createdTime: todoItem.CreatedTime);
+                return new TodoItemDto(
+                        id: todoItem.Id,
+                        text: todoItem.Text,
+                        isCompleted: todoItem.IsCompleted,
+                        createdTime: todoItem.CreatedTime);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in CreateTodo:{ex.Message}");
+                throw;
+            }
+
         }
 
         public async Task<bool> DeleteTodo(int id)
         {
-            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
-            if (todoItem == null)
+            try
             {
-                return false;
+                var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+                if (todoItem == null)
+                {
+                    return false;
+                }
+
+                _context.TodoItems.Remove(todoItem);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in DeleteTodo:{ex.Message}");
+                throw;
             }
 
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
-            return true;
 
         }
 
         public async Task<PagintionListDto<TodoItemDto>> GetAll(int page, int pageSize, bool? isComleted)
         {
-            IQueryable<TodoItem> todoQuery=_context.TodoItems.AsQueryable();
-            if (isComleted.HasValue)
+            try
             {
-                todoQuery = todoQuery.Where(e => e.IsCompleted == isComleted);
+                IQueryable<TodoItem> todoQuery = _context.TodoItems.AsQueryable();
+                if (isComleted.HasValue)
+                {
+                    todoQuery = todoQuery.Where(e => e.IsCompleted == isComleted);
+                }
+                var items = await todoQuery.Skip((page - 1) - pageSize).Take(pageSize).ToListAsync();
+                var totalCount = await todoQuery.CountAsync();
+                return new PagintionListDto<TodoItemDto>(
+
+                    items.Select(e => new TodoItemDto(
+                        id: e.Id,
+                        text: e.Text,
+                        isCompleted: e.IsCompleted,
+                        createdTime: e.CreatedTime
+
+                        )),
+                    new PagintionMeta(page, pageSize, totalCount)
+
+                    );
+
             }
-            var items= await todoQuery.Skip((page - 1)-pageSize).Take(pageSize).ToListAsync();
-            var totalCount=await todoQuery.CountAsync();
-            return new PagintionListDto<TodoItemDto>(
-
-                items.Select(e=> new TodoItemDto(
-                    id:e.Id,
-                    text:e.Text,
-                    isCompleted:e.IsCompleted,
-                    createdTime:e.CreatedTime
-
-                    )),
-                new PagintionMeta(page,pageSize,totalCount)
-
-                );
-
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in GetAll:{ex.Message}");
+                throw;
+            }
         }
 
 
         public async Task<TodoItemDto?> GetTodoItem(int id)
         {
-            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
+            try
+            {
+                var todoItem = await _context.TodoItems.FirstOrDefaultAsync(e => e.Id == id);
 
-            return todoItem is not null
-                ? new TodoItemDto(
-                    id: todoItem.Id,
-                    text: todoItem.Text,
-                    isCompleted: todoItem.IsCompleted,
-                    createdTime: todoItem.CreatedTime)
-                : null;
+                return todoItem is not null
+                    ? new TodoItemDto(
+                        id: todoItem.Id,
+                        text: todoItem.Text,
+                        isCompleted: todoItem.IsCompleted,
+                        createdTime: todoItem.CreatedTime)
+                    : null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in GetTodoItem:{ex.Message}");
+                throw;
+            }
         }
     }
 }
